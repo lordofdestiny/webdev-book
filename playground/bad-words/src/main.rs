dc045f83de92af1c3ef491762e7c9ac613ec0a23
lod <djumicbranislav@outlook.com>
2024-02-24 15:22:31 +0100

    Updated Bad Words API interface

--- playground/bad-words/src/main.rs
+++ playground/bad-words/src/main.rs
@@ -1,3 +1,4 @@
+use reqwest::header::HeaderMap;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -42,13 +43,13 @@ impl BadWords {
 
     pub fn restore<F>(&mut self, predicate: F)
     where
-        F: FnOnce(&BadWord) -> bool + Clone,
+        F: FnMut(&&BadWord) -> bool,
     {
-        for i in 0..self.bad_words_list.len() {
-            if let Some(value) = self.restore_word(i, predicate.clone()) {
-                self.censored_content = value;
-            }
-        }
+        self.bad_words_list.iter().filter(predicate).for_each(
+            |BadWord {
+                 start, end, original, ..
+             }| { self.censored_content.replace_range(*start..*end, original) },
+        )
     }
 
     pub fn apply_word<F>(&self, index: usize, predicate: F) -> Option<String>
@@ -64,23 +65,21 @@ impl BadWords {
         let BadWord { start, end, .. } = bad_word;
         let mut new_str = self.censored_content.clone();
         let replacement = String::from(self.censor_char).repeat(end - start);
-        if *end == self.content.len() {
-            new_str.replace_range(*start.., &replacement);
-        } else {
-            new_str.replace_range(*start..*end, &replacement);
-        };
+        new_str.replace_range(*start..*end, &replacement);
         Some(new_str)
     }
 
     pub fn apply<F>(&mut self, predicate: F)
     where
-        F: FnOnce(&BadWord) -> bool + Clone,
+        F: FnMut(&&BadWord) -> bool,
     {
-        for i in 0..self.bad_words_list.len() {
-            if let Some(value) = self.apply_word(i, predicate.clone()) {
-                self.censored_content = value;
-            }
-        }
+        self.bad_words_list
+            .iter()
+            .filter(predicate)
+            .for_each(|BadWord { start, end, .. }| {
+                let replacement = String::from(self.censor_char).repeat(end - start);
+                self.censored_content.replace_range(*start..*end, &replacement)
+            })
     }
 }
 
@@ -111,32 +110,44 @@ impl BadWordsAPIResponse {
 
 struct BadWordsAPI {
     censor_char: char,
-    api_key: String,
     url: String,
     client: reqwest::Client,
+}
+
+#[derive(thiserror::Error, Debug)]
+enum BadWordsAPIBuildError {
+    #[error("invalid header value: {0}")]
+    BadAPIKeyValue(#[from] reqwest::header::InvalidHeaderValue),
+    #[error("failed to build client: {0}")]
+    ClientBuildError(#[from] reqwest::Error),
+}
+
+#[derive(thiserror::Error, Debug)]
+enum BadWordsAPIError {
+    #[error("error with executing the request")]
+    RequestError(#[from] reqwest::Error),
+    #[error("error with deserializing json response")]
+    DeserializationError(#[from] serde_json::Error),
+    #[error("header field contains invalid unicode")]
+    BadHeaderValue(#[from] reqwest::header::ToStrError),
 }
 
 impl BadWordsAPI {
     const API_ENDPOINT: &'static str = "https://api.apilayer.com/bad_words";
 
-    pub fn new(api_key: String, censor_char: char) -> Self {
-        BadWordsAPI {
+    pub fn build(api_key: String, censor_char: char) -> Result<Self, BadWordsAPIBuildError> {
+        let mut headers = HeaderMap::new();
+        headers.insert("apikey", api_key.parse()?);
+
+        Ok(BadWordsAPI {
             censor_char,
-            api_key,
             url: format!("{}?censor_character={censor_char}", Self::API_ENDPOINT),
-            client: reqwest::Client::new(),
-        }
+            client: reqwest::Client::builder().default_headers(headers).build()?,
+        })
     }
 
     pub async fn censor(&self, text: &str) -> Result<BadWordsAPIResponse, BadWordsAPIError> {
-        let response = match self
-            .client
-            .post(&self.url)
-            .header("apikey", &self.api_key)
-            .body(text.to_owned())
-            .send()
-            .await
-        {
+        let response = match self.client.post(&self.url).body(text.to_owned()).send().await {
             Ok(response) => response,
             Err(e) => {
                 eprintln!("censoring request failed: {e}");
@@ -154,9 +165,7 @@ impl BadWordsAPI {
             }
         };
 
-        let result: Result<BadWordsAPIResponse, _> = serde_json::from_str(&response_body);
-
-        match result {
+        match serde_json::from_str::<BadWordsAPIResponse>(&response_body) {
             Ok(mut bad_words) => {
                 bad_words.finalize(status_code, self.censor_char);
                 Ok(bad_words)
@@ -169,32 +178,27 @@ impl BadWordsAPI {
     }
 }
 
-#[derive(thiserror::Error, Debug)]
-enum BadWordsAPIError {
-    #[error("error with executing the request")]
-    RequestError(#[from] reqwest::Error),
-    #[error("error with deserializing json response")]
-    DeserializationError(#[from] serde_json::Error),
-    #[error("header field contains invalid unicode")]
-    BadHeaderValue(#[from] reqwest::header::ToStrError),
-}
-
 #[tokio::main]
-async fn main() -> Result<std::process::ExitCode, BadWordsAPIError> {
+async fn main() -> Result<std::process::ExitCode, Box<dyn std::error::Error>> {
     let api_key = std::env::var("API_LAYER_KEY").unwrap_or("KHFM68sNbwzuPqELmjnevUv2k17EdiBE".to_owned());
     // .expect("API_LAYER_KEY not found");
 
-    let api = BadWordsAPI::new(api_key, '*');
+    let api = BadWordsAPI::build(api_key, '*')?;
     let result = api.censor("a list of shit words, you son of a bitch").await?;
 
     match result {
         BadWordsAPIResponse::BadWordsResponse(mut bad_words) => {
             println!("Censored: {}", bad_words.censored_content);
             bad_words.restore(|word| word.original.contains("son of a"));
+            println!("Restored: {}", bad_words.censored_content);
             bad_words.restore(|word| word.original.contains("bitch"));
             println!("Restored: {}", bad_words.censored_content);
+            bad_words.restore(|_| true);
+            println!("Restored: {}", bad_words.censored_content);
+
             bad_words.apply(|_| true);
             println!("Censored: {}", bad_words.censored_content);
+
             Ok(std::process::ExitCode::SUCCESS)
         }
         BadWordsAPIResponse::ErrorResponse { message, status_code } => {
